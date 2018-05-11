b531e08c3400dde965a9cc4636a090859077873b
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-11 01:30:23 -0400

    updates 02

--- R/asdar/02_spatialDataClasses.R
+++ R/asdar/02_spatialDataClasses.R
@@ -1,6 +1,8 @@
 rm(list=ls())
 setwd('~/Learning/spatial/R/asdar')
 
+library(maps)
+library(maptools)
 library(sp)
 
 
@@ -18,21 +20,96 @@ Spatial(bb, proj4string=CRS('+proj=longlat'))
 
 
 # 4. Spatial Points
-CRAN.df <- read.table('data/CRAN051001a.txt', header=T)
-CRAN.mat <- cbind(CRAN.df$long, CRAN.df$lat)
-row.names(CRAN.mat) <- 1:nrow(CRAN.mat)
-str(CRAN.mat)
+cran.df <- read.table('data/CRAN051001a.txt', header=T)
+cran.mat <- cbind(cran.df$long, cran.df$lat)
+row.names(cran.mat) <- 1:nrow(cran.mat)
+str(cran.mat)
 getClass('SpatialPoints')
 llCRS <- CRS('+proj=longlat +ellps=WGS84')
-CRAN.sp <- SpatialPoints(CRAN.mat, proj4string=llCRS)
-summary(CRAN.sp)
+cran.sp <- SpatialPoints(cran.mat, proj4string=llCRS)
+summary(cran.sp)
 
 # 4.1 Methods
-bbox(CRAN.sp)
-proj4string(CRAN.sp)
-brazil <- which(CRAN.df$loc == 'Brazil')
+bbox(cran.sp)
+proj4string(cran.sp)
+brazil <- which(cran.df$loc == 'Brazil')
 brazil
-coordinates(CRAN.sp)[brazil,]
-summary(CRAN.sp[brazil,])
-southern.hemi <- which(coordinates(CRAN.sp)[, 2] < 0)
-summary(CRAN.sp[-southern.hemi, ])
+coordinates(cran.sp)[brazil,]
+summary(cran.sp[brazil,])
+southern.hemi <- which(coordinates(cran.sp)[, 2] < 0)
+summary(cran.sp[-southern.hemi, ])
+
+# 4.2 Dataframes for Spatial Point Data
+str(row.names(cran.df))
+cran.spdf1 <- SpatialPointsDataFrame(
+  cran.mat, cran.df, proj4string=llCRS, match.ID=T)
+str(cran.spdf1)
+cran.spdf1[4,]
+cran.spdf1@data[4, ]
+cran.spdf1$loc # or [['loc']]
+s <- sample(nrow(cran.df))
+cran.spdf2 <- SpatialPointsDataFrame(
+  cran.mat, cran.df[s,], proj4string=llCRS, match.ID=T)
+all.equal(cran.spdf1, cran.spdf2)
+
+cran.df1 <- cran.df
+row.names(cran.df1) <- sample(c(outer(letters, letters, paste, sep='')), 
+                              nrow(cran.df1))
+cran.spdf3 <- SpatialPointsDataFrame(
+  cran.mat, cran.df1, proj4string=llCRS, match.ID=T) # Throws error
+  
+getClass('SpatialPointsDataFrame')
+names(cran.spdf1)
+str(model.frame(lat ~ long, data=cran.spdf1), give.attr=F)
+
+cran.spdf4 <- SpatialPointsDataFrame(cran.sp, cran.df)
+all.equal(cran.spdf4, cran.spdf2)
+
+cran.df0 <- cran.df
+coordinates(cran.df0) <- cran.mat
+proj4string(cran.df0) <- llCRS
+all.equal(cran.df0, cran.spdf2)
+
+str(cran.df0, max.level=2)
+cran.df1 <- cran.df
+names(cran.df1)
+coordinates(cran.df1) <- c('long', 'lat')
+proj4string <- llCRS
+str(cran.df1, max.level=2)
+
+turtle.df <- read.csv('data/seamap105_mod.csv')
+head(turtle.df)
+timestamp <- as.POSIXlt(
+  strptime(as.character(turtle.df$obs_date), '%m/%d/%Y %H:%M:%S'), 
+  'GMT')
+turtle.df1 <- data.frame(turtle.df, timestamp=timestamp)
+turtle.df1$lon <- ifelse(turtle.df1$lon < 0, turtle.df1$lon + 360, turtle.df$lon)
+turtle.sp <- turtle.df1[order(turtle.df1$timestamp), ]
+coordinates(turtle.sp) <- c('lon', 'lat')
+proj4string(turtle.sp) <- CRS('+proj=longlat +ellps=WGS84')
+plot(turtle.sp)
+map('world', add=T)
+
+
+# 5 SpatialLines
+getClass('Line')
+getClass('Lines')
+getClass('SpatialLines')
+
+japan <- map('world', 'japan', plot=F)
+p4s <- CRS('+proj=longlat +ellps=WGS84')
+SLjapan <- map2SpatialLines(japan, proj4string=p4s)
+str(SLjapan, max.level=2)
+plot(SLjapan)
+
+lines.len <- sapply(slot(SLjapan, 'lines'), function(x) length(slot(x, 'Lines')))
+table(lines.len)
+
+volcano.s1 <- ContourLines2SLDF(contourLines(volcano))
+plot(volcano.s1)
+t(slot(volcano.s1, 'data'))
+
+llCRS <- CRS('+proj=longlat +ellps=WGS84')
+auck.shore <- MapGen2SL('data/auckland_mapgen.dat', llCRS)
+plot(auck.shore)
+summary(auck.shore)
