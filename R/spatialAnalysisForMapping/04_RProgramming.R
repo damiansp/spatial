598acb51c90bbcebc086ec7bad4591555816289c
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2018-05-20 14:03:00 -0400

    fin 4

--- R/spatialAnalysisForMapping/04_RProgramming.R
+++ R/spatialAnalysisForMapping/04_RProgramming.R
@@ -19,10 +19,23 @@ invisible(lapply(georgia.polys, polygon)) # prevents ouput
 poly1 <- georgia.polys[[1]]
 min(poly1[, 1])
 
-get.eastmost <- function(polys) { 
+get.bounds <- function(polys) { 
   eastmost.list <- lapply(polys, function(poly) { min(poly[, 1]) })
-  min(unlist(eastmost.list))
+  westmost.list <- lapply(polys, function(poly) { max(poly[, 1]) })
+  ew <- c(min(unlist(eastmost.list)), max(unlist(westmost.list)))
+  northmost.list <- lapply(polys, function(poly) { max(poly[1, ]) })
+  southmost.list <- lapply(polys, function(poly) { min(poly[1, ]) })
+  ns <- c(min(unlist(southmost.list)), max(unlist(northmost.list)))
+  list(ew=ew, ns=ns)
 }
 
-get.eastmost(georgia.polys)
+ga.bounds <- get.bounds(georgia.polys)
 
+# 5.3 Shaded Maps
+classifier <- factor(ifelse(georgia$PctRural > 50, 'rural', 'urban'))
+fill.cols <- character(length(classifier))
+fill.cols[classifier == 'urban'] <- 'yellow'
+fill.cols[classifier == 'rural'] <- 'darkgreen'
+par(mar=rep(0, 4))
+plot(ga.bounds$ew, ga.bounds$ns, asp=1, type='n')
+invisible(mapply(polygon, georgia.polys, col=fill.cols))
