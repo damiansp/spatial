a08c7c5a98bb884a8e1991a4eb94b46902afc995
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2019-07-18 00:59:18 -0400

    reformat

--- R/asdar/04_spatialDataImportExport.R
+++ R/asdar/04_spatialDataImportExport.R
@@ -1,5 +1,9 @@
 #---------#---------#---------#---------#---------#---------#---------#---------
 rm(list=ls())
+lapply(paste('package:', names(sessionInfo()$otherPkgs), sep=''),
+       detach,
+       character.only=T,
+       unload=T)
 setwd('~/Learning/spatial/R/asdar')
 
 library(DCluster)
@@ -8,7 +12,10 @@ library(rgdal)
 library(rgeos)
 library(spacetime)
 library(spdep)
+
 data(wrld_simpl)
+
+
 
 # 1 Coordinate Reference Systems
 epsg.news <- 'http://svn.osgeo.org/metacrs/proj/trunk/proj/NEWS'
@@ -16,11 +23,13 @@ proj4.news <- readLines(url(epsg.news))
 lines <- grep('Release Notes|EPSG', proj4.news)
 head(proj4.news[lines])
 
+
 # 1.1 Using the EPSG list
 EPSG <- make_EPSG()
 head(EPSG)
 class(EPSG)
 EPSG[grep('^# ED50$', EPSG$note), ]
+
 
 # 1.2 PROJ.4 CRS specifications
 CRS('+init=epsg:4230')
